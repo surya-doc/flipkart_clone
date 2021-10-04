2205eb027f3f72d97c2427182c0e03300d3b1ff5
surya-doc <suryadanabnk@gmail.com>
2021-10-04 19:24:41 +0530

    :zap: added ActionType.js

---
+++ client/src/ActionType.js
@@ -0,0 +1,12 @@
+export const UPDATE_STORE = 'UPDATE_STORE';
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const SIGNIN = 'SIGNIN';
+export const SAVE_ADDRESS = 'SAVE_ADDRESS';
+export const CART_UPDATE = 'CART_UPDATE';
+export const CART_UPDATE_WITH_USER = 'CART_UPDATE_WITH_USER';
+export const PLACE_ORDER = "PLACE_ORDER";
+export const ERROR_STATUS = "ERROR_STATUS";
+export const SET_ADDRESS = "SET_ADDRESS";
+export const ORDERD_ITEMS = "ORDERD_ITEMS";
+export const ADD_ITEMS_TO_REDUX = "ADD_ITEMS_TO_REDUX";
+export const LOG_OUT = "LOG_OUT";
