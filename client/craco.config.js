3d8f40a83099745d7480ddc2b68a493375431a5d
surya-doc <suryadanabnk@gmail.com>
2021-10-04 19:18:10 +0530

    :tada: adding public folder and packages for frontend

---
+++ client/craco.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    style: {
+      postcss: {  
+        purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+        darkMode: false, // or 'media' or 'class'
+        theme: {
+          extend: {},
+        },
+        variants: {
+          extend: {},
+        },
+        plugins: [
+          require('tailwindcss'),
+          require('autoprefixer'),
+        ],
+      },
+    },
+  }
